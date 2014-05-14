2a8c625de6aedb07e5026cc34cca61756fb5b437
Vicky Gong <vickyg@mit.edu>
2014-05-14 05:09:02 -0400

    about and index

--- js/navbar.js
+++ js/navbar.js
@@ -10,15 +10,15 @@
 
 
 
-var add_navbar = function(divId){
+var add_navbar = function(divId, activeId){
   var navBarOpener = '<div id="navbar">';
-  var logo = '<div id="logo">MOODLY</div>';
+  var logo = '<a href="index.html" class="link"><div id="logo">MOODLY</div> </a>';
 
   var menu = '<div id="menu">';
-  menu += '<div class="menuItemBox"><a href="index.html" class="menuItem">About</a></div>';
-  menu += '<div class="menuItemBox"><a href="ethnographic.html" class="menuItem">Ethnographic studies</a></div>';
-  menu += '<div class="menuItemBox"><a href="concept.html" class="menuItem">Concept</a></div>';
-  menu += '<div class="menuItemBox"><a href="howitworks.html" class="menuItem">How it works</a></div>';
+  menu += '<div id="navAbout" class="menuItemBox"><a href="about.html" class="menuItem">About</a></div>';
+  menu += '<div id="navEthno" class="menuItemBox"><a href="ethnographic.html" class="menuItem">Ethnographic studies</a></div>';
+  menu += '<div id="navConcept" class="menuItemBox"><a href="concept.html" class="menuItem">Concept</a></div>';
+  menu += '<div id="navWorks" class="menuItemBox"><a href="howitworks.html" class="menuItem">How it works</a></div>';
   menu += '</div>';
             
   var navBarCloser = '</div>';
@@ -26,5 +26,8 @@ var add_navbar = function(divId){
   var navBarHtml = navBarOpener + logo + menu + navBarCloser;
 
   $(divId).prepend(navBarHtml);
+  $(activeId).addClass('navActive');
+
+
 }
 
